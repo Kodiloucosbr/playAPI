57996466f6c1f53244d2b01206cfde1e6dbfa11e
Kodiloucosbr <107689677+Kodiloucosbr@users.noreply.github.com>
2025-12-08 14:36:20 -0300

    Update base URL and improve error handlingup
    up

--- api_vod.py
+++ api_vod.py
@@ -14,7 +14,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:141.0) Ge
 
 class VOD:
     def __init__(self):
-        self.base = 'https://superflixapi.asia'
+        self.base = 'https://superflixapi.run'
 
     def tvshows(self,imdb,season,episode):
         stream = ''
@@ -98,6 +98,7 @@ class VOD:
 #print(VOD().movie('tt0106697'))
 #print(VOD().tvshows('tt3107288','1','1'))
     
+
 
 
 
